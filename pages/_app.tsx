f359e175c1e3bcb92a3227237403fb54cd517554
Cheuktingchan <cheuktingchan@outlook.com>
2022-12-14 16:02:44 +0000

    feat: :sparkles: implemented quickstart authentication

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { supabase } from "../utils/supabase";
+import { useEffect, useState } from "react";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    const [supabase] = useState(() => createBrowserSupabaseClient());
+    return (
+        <SessionContextProvider
+            supabaseClient={supabase}
+            initialSession={pageProps.initialSession}
+        >
+            <Component {...pageProps} />
+        </SessionContextProvider>
+    );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,28 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { supabase } from "../utils/supabase";
+import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
+import Account from "../components/Account";
 
-export default function Home() {
-    return <h1>Mini Workout Planner</h1>;
-}
+const Home = () => {
+    const session = useSession();
+    const supabase = useSupabaseClient();
+
+    return (
+        <div className="container" style={{ padding: "50px 0 100px 0" }}>
+            {!session ? (
+                <Auth
+                    supabaseClient={supabase}
+                    appearance={{ theme: ThemeSupa }}
+                    theme="dark"
+                />
+            ) : (
+                <Account session={session} />
+            )}
+        </div>
+    );
+};
+
+export default Home;
