1cd1051bd3037c8f1eff23176ff67e2145d69922
Cheuktingchan <cheuktingchan@outlook.com>
2023-08-24 17:36:32 +0100

    feat: :sparkles: added user_exercise table and user_exercise creation page

---
+++ pages/create/new_exercise.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import styles from "../../styles/Create.module.css";
+import { useRouter } from "next/router";
+import {
+    useSession,
+    useSupabaseClient,
+    useUser,
+} from "@supabase/auth-helpers-react";
+import Navbar from "../../components/Navbar";
+
+const CreateNewUserExercise = () => {
+    const session = useSession();
+    const initialState = {
+        title: "Exercise",
+    };
+    const supabase = useSupabaseClient();
+    const user = useUser();
+    const router = useRouter();
+    const [exerciseData, setExerciseData] = useState(initialState);
+
+    const { title } = exerciseData;
+
+    const handleChange = (e: any) => {
+        setExerciseData({ ...exerciseData, [e.target.name]: e.target.value });
+    };
+
+    const createExercise = async () => {
+        const { data, error, status } = await supabase
+            .from("user_exercise")
+            .insert({
+                title,
+                user_created: user?.id,
+            })
+            .single();
+        console.log(error, data);
+        setExerciseData(initialState);
+        router.push("/");
+    };
+
+    return (
+        <>
+            <Navbar session={session}></Navbar>
+            <div className={styles.container}>
+                <div className={styles.form}>
+                    <p className={styles.title}>Create a New Exercise</p>
+                    <label className={styles.label}>Exercise:</label>
+                    <input
+                        type="text"
+                        name="title"
+                        value={title}
+                        onChange={handleChange}
+                        className={styles.input}
+                        placeholder="Enter a title"
+                    />
+                    <button className={styles.button} onClick={createExercise}>
+                        Create Exercise
+                    </button>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default CreateNewUserExercise;
